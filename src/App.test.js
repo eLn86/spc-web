0850f5361f5f582edafdf5526f47a264c2a24a2e
Elnathan Erh <elnathan.erh@synpulse.com>
2024-03-13 14:49:38 +0800

    Add intial App tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders header text', () => {
-  render(<App />);
-  const headerText = screen.getByText(/scrabble points calculator deluxe/i);
-  expect(headerText).toBeInTheDocument();
-});
+describe('Main page text', () => {
+  test('renders header text', () => {
+    render(<App />);
+    const headerText = screen.getByText(/scrabble points calculator deluxe/i);
+    expect(headerText).toBeInTheDocument();
+  });
 
+  test('renders footer text', () => {
+    render(<App />);
+    const footerText = screen.getByText(/cognizant gobiz technical challenge march 2024/i);
+    expect(footerText).toBeInTheDocument();
+  })
+})
