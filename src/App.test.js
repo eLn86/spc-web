b90c58e8fe0ad566aa3e745b917fa119c6ae8a7e
Elnathan Erh <elnathan.erh@synpulse.com>
2024-03-19 17:20:59 +0800

    add new test for navigation - checking initial loaded page

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import Main from "./pages/Main/Main";
+import TopScores from "./pages/TopScores/TopScores";
 
 describe('App layout text', () => {
   test('renders header text', () => {
@@ -12,5 +15,21 @@ describe('App layout text', () => {
     render(<App />);
     const footerText = screen.getByText(/cognizant gobiz technical challenge march 2024/i);
     expect(footerText).toBeInTheDocument();
+  })
+})
+
+describe('Navigation', () => {
+  test('renders Main component as initial route', () => {
+    render(
+        <MemoryRouter>
+          <Routes>
+            <Route path='/' element={<Main />} />
+            <Route path='/top-scores' element={<TopScores />} />
+          </Routes>
+        </MemoryRouter>
+    );
+
+    const resetTilesBtn = screen.getByTestId('reset-tiles-btn');
+    expect(resetTilesBtn).toBeInTheDocument();
   })
 })
