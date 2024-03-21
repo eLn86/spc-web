c8f88d556205d03ec1ebd8dc6cd81d07cbff90f6
Elnathan Erh <elnathan.erh@synpulse.com>
2024-03-21 20:26:02 +0800

    extract request headers to common file

---
+++ src/apis/common.js
@@ -0,0 +1,7 @@
+export const REQUEST_HEADERS = {
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+    },
+}
