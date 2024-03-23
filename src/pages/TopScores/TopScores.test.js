2778d53d7ff43aa7511372315cee1b3eb082feed
Elnathan Erh <elnathan.erh@synpulse.com>
2024-03-23 21:47:46 +0800

    refactor existing tests and add more tests to test navigation and API behaviour.

--- src/pages/TopScores/TopScores.test.js
+++ src/pages/TopScores/TopScores.test.js
@@ -1,9 +1,11 @@
 import * as scoresAPI from "../../apis/scoresAPI";
-import { act, findAllByText, render, screen } from "@testing-library/react";
+import { act, fireEvent, render, screen, waitFor } from "@testing-library/react";
 import TopScores from "./TopScores";
 import '../../setupDomTests';
-import { TABLE_ROLES } from "../../constants";
+import { LABELS, TABLE_ROLES } from "../../constants";
 import React from "react";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import Main from "../Main/Main";
 
 jest.mock("../../apis/scoresAPI");
 
@@ -23,29 +25,58 @@ beforeEach(async () => {
 });
 describe('TopScores Page UI', () => {
     test('renders header text', async () => {
-        await act(async () => render(<TopScores/>));
+        render(
+            <MemoryRouter>
+                <TopScores/>
+            </MemoryRouter>
+        );
         const headerText = screen.getByText(/top 10 scores/i);
-        expect(headerText).toBeInTheDocument();
+        const backButton = screen.getByTestId('back-to-home-btn');
+        await waitFor(() => {
+            expect(headerText).toBeInTheDocument();
+            expect(backButton).toBeInTheDocument();
+        });
     });
 
     test('render table with Word and Score columns', async () => {
-        await act(async () => render(<TopScores/>));
+        render(
+            <MemoryRouter>
+                <TopScores/>
+            </MemoryRouter>
+        );
         const table = screen.getByTestId('score-table');
-        expect(table).toBeInTheDocument();
+        await waitFor(() => {
+            expect(table).toBeInTheDocument();
+        });
+
         const headerCellWithWordText = screen.getByRole(TABLE_ROLES.COLUMN_HEADER, { name: 'Word Submitted' });
+        const headerCellWithRankText = screen.getByRole(TABLE_ROLES.COLUMN_HEADER, { name: 'Rank' });
         const headerCellWithScoreText = screen.getByRole(TABLE_ROLES.COLUMN_HEADER, { name: 'Score Attained' });
+        expect(headerCellWithRankText).toBeInTheDocument();
         expect(headerCellWithWordText).toBeInTheDocument();
         expect(headerCellWithScoreText).toBeInTheDocument();
     })
+})
 
+describe('API behavior and resultant UI', () => {
     test('renders top scores page with data provided by API (mocked)', async () => {
-        await act(async () => render(<TopScores/>));
+        render(
+            <MemoryRouter>
+                <TopScores/>
+            </MemoryRouter>
+        );
         expect(await screen.findByText('magnum')).toBeVisible();
     })
 
     test('remaining number of slots to be 10 - apiData.length (length is mocked to be 3)', async () => {
-        const { findAllByText } = await act(async () => render(<TopScores/>));
-        expect(await findAllByText('This slot has yet to be filled!')).toHaveLength(7);
+        render(
+            <MemoryRouter>
+                <TopScores/>
+            </MemoryRouter>
+        );
+        await waitFor(async () => {
+            expect(await screen.findAllByText('This slot has yet to be filled!')).toHaveLength(7);
+        });
     })
 
     test('render 10 slots with This slot has yet to be filled! when no api.data', async () => {
@@ -55,7 +86,34 @@ describe('TopScores Page UI', () => {
             }
         };
         scoresAPI.getTopTenScores.mockResolvedValue(mockScores);
-        const { findAllByText } = await act(async () => render(<TopScores/>));
-        expect(await findAllByText('This slot has yet to be filled!')).toHaveLength(10);
+        render(
+            <MemoryRouter>
+                <TopScores/>
+            </MemoryRouter>
+        );
+        await waitFor(async () => {
+            expect(await screen.findAllByText('This slot has yet to be filled!')).toHaveLength(10);
+        });
+    })
+
+    test('navigate to home page from top scores page when Back to Home button is clicked', async () => {
+        render(
+            <MemoryRouter initialEntries={['/top-scores']}>
+                <Routes>
+                    <Route path='/' element={<Main/>}/>
+                    <Route path='/top-scores' element={<TopScores/>}/>
+                </Routes>
+            </MemoryRouter>
+        );
+
+        const backButton = screen.getByTestId('back-to-home-btn');
+        act(() => {
+            // Navigate to top scores page by clicking on View Top Scores button
+            fireEvent.click(backButton);
+        });
+
+        await waitFor(() => {
+            expect(screen.getByTestId('welcome-text')).toHaveTextContent(LABELS.WELCOME_TEXT);
+        });
     })
 })
